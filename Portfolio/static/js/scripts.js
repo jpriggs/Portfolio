e9e3d68d8fdc909c356a289bba5e5d5dd03e3ed4
jpriggs <jprigdon@gmail.com>
2018-09-10 00:14:09 +0900

    Fixed issues with library loading and css errors

--- Portfolio/static/js/scripts.js
+++ Portfolio/static/js/scripts.js
@@ -2,9 +2,16 @@ var thumbnails = document.getElementsByClassName("image-container");
 
 var getThumbnail = function() {
   var url = this.getElementsByTagName("A")[0].getAttribute("href");
+  console.log(url);
   window.open(url);
 }
 
-Array.from(thumbnails).forEach(function(element) {
-  element.addEventListener("click", getThumbnail);
-});
+for (var i = 0; i < thumbnails.length; i++) {
+    console.log(thumbnails[i]);
+    thumbnails[i].addEventListener("click", getThumbnail, false);
+}
+
+/*Array.from(thumbnails).forEach(function(element) {
+    console.log(element);
+    element.addEventListener("click", getThumbnail);
+});*/
